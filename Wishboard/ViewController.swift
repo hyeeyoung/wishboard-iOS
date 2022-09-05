9ef497b10f9f63355392639c888650c623c3637e
gom1n <alice436777@gmail.com>
2022-09-05 10:41:45 +0900

    Initial Commit

---
+++ Wishboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wishboard
+//
+//  Created by gomin on 2022/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
