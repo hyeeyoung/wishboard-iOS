8a6a5ff9d68d38e323dc1107708fdd3d8781484f
gom1n <alice436777@gmail.com>
2022-09-15 14:21:01 +0900

    [fix] 마이페이지 UI 수정 및 Textfield를 포함한 팝업창 띄우기 (회원탈퇴)

--- Wishboard/domain/ViewControllers/MyPageViewController.swift
+++ Wishboard/domain/ViewControllers/MyPageViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class MyPageViewController: UIViewController {
     var mypageView: MyPageView!
-    let settingArray = ["설정", "알림", "프로필 설정", "계정관리", "로그아웃", "탈퇴하기", "기타", "버전 정보", "이용약관", "자주 묻는 질문"]
+    let settingArray = ["설정", "알림 설정", "고객 지원", "문의하기", "서비스 정보", "위시보드 이용 방법", "이용약관", "버전 정보", "계정 관리", "로그아웃", "회원 탈퇴"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +33,7 @@ class MyPageViewController: UIViewController {
 // MARK: - Main TableView delegate
 extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 11
+        return 12
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let tag = indexPath.row
@@ -44,10 +44,10 @@ extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
         } else {
             let cell = UITableViewCell()
             cell.textLabel?.text = settingArray[tag - 1]
-            if tag == 1 || tag == 4 || tag == 7 {self.setTitleConstraint(cell)}
+            if tag == 1 || tag == 3 || tag == 5 || tag == 9 {self.setTitleConstraint(cell)}
             else {
                 cell.textLabel?.font = UIFont.Suit(size: 12, family: .Regular)
-                cell.textLabel?.textColor = .gray
+                cell.textLabel?.textColor = .black
                 if tag == 2 {self.setSwitch(cell)}
                 if tag == 8 {self.setVersionLabel(cell)}
             }
@@ -61,22 +61,32 @@ extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
         switch tag {
         case 0:
             return 184
-        case 1, 4, 7:
+        case 1, 3, 5, 9:
             return 62
+        case 2:
+            return 76
         default:
             return 40
         }
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
+        let tag = indexPath.row
+        switch tag {
+        case 0:
             let modifyProfile = ModifyProfileViewController()
             modifyProfile.modalPresentationStyle = .fullScreen
             self.present(modifyProfile, animated: true, completion: nil)
+        case 10:
+            showLogoutDialog()
+        case 11:
+            showSignoutDialog()
+        default:
+            tableView.deselectRow(at: indexPath, animated: true)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
-// MARK: setting cell set
+// MARK: setting cell set & Functions
 extension MyPageViewController {
     func setTitleConstraint(_ cell: UITableViewCell) {
         cell.textLabel!.snp.makeConstraints { make in
@@ -89,17 +99,28 @@ extension MyPageViewController {
         let notiSwitch = UISwitch().then{
             $0.isOn = true
             $0.onTintColor = .wishboardGreen
+            $0.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+        }
+        let subTitleLabel = UILabel().then{
+            $0.text = "알림 설정을 켜면 상품의 재입고, 프리오더 시작일 등 상품 일정을 알림 받을 수 있어요!"
+            $0.font = UIFont.Suit(size: 8, family: .Regular)
+            $0.textColor = .wishboardGray
         }
         cell.addSubview(notiSwitch)
+        cell.addSubview(subTitleLabel)
         notiSwitch.snp.makeConstraints { make in
             make.trailing.equalToSuperview().offset(-16)
             make.centerY.equalToSuperview()
+        }
+        subTitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().offset(-10)
         }
     }
     func setVersionLabel(_ cell: UITableViewCell) {
         let versionLabel = UILabel().then{
             $0.text = "1.0.0"
-            $0.font = UIFont.Suit(size: 12, family: .Regular)
+            $0.font = UIFont.Suit(size: 12, family: .Bold)
             $0.textColor = .gray
         }
         cell.addSubview(versionLabel)
@@ -107,5 +128,15 @@ extension MyPageViewController {
             make.trailing.equalToSuperview().offset(-16)
             make.centerY.equalToSuperview()
         }
+    }
+    func showLogoutDialog() {
+        let dialog = PopUpViewController(titleText: "로그아웃", messageText: "정말 로그아웃 하시겠어요?", greenBtnText: "취소", blackBtnText: "로그아웃")
+        dialog.modalPresentationStyle = .overCurrentContext
+        self.present(dialog, animated: false, completion: nil)
+    }
+    func showSignoutDialog() {
+        let dialog = PopUpTextfieldViewController(titleText: "회원 탈퇴", messageText: "탈퇴하시면 회원정보는 7일 후 파기됩니다.", greenBtnText: "취소", blackBtnText: "탈퇴", placeholder: "닉네임을 입력해주세요.")
+        dialog.modalPresentationStyle = .overCurrentContext
+        self.present(dialog, animated: false, completion: nil)
     }
 }
