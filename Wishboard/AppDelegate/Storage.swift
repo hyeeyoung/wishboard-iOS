27dd608670bbeafadda76a358f46b0faba84c458
gom1n <alice436777@gmail.com>
2022-11-08 19:56:41 +0900

    [feat] Add Storage to gitignore

---
+++ Wishboard/AppDelegate/Storage.swift
@@ -0,0 +1,13 @@
+//
+//  Storage.swift
+//  Wishboard
+//
+//  Created by gomin on 2022/11/08.
+//
+
+import Foundation
+
+struct Storage {
+    let BaseURL = "http://3.39.165.250:3000"
+    let ShareExtension = "group.gomin.Wishboard.Share"
+}
