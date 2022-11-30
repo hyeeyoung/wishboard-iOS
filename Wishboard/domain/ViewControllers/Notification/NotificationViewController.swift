c2c2873c5a6e1fa6067f0ef7b92cc639eba15d5b
gom1n <alice436777@gmail.com>
2022-11-30 09:59:23 +0900

    [feat] 알림탭 새로고침 기능 추가

--- Wishboard/domain/ViewControllers/Notification/NotificationViewController.swift
+++ Wishboard/domain/ViewControllers/Notification/NotificationViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NotificationViewController: TitleLeftViewController {
     var notiView: NotiView!
+    var refreshControl = UIRefreshControl()
+    
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,9 @@ class NotificationViewController: TitleLeftViewController {
         }
         // DATA
         NotificationDataManager().getNotificationListDataManager(self.notiView)
+        refreshControl.endRefreshing()
+        // Refresh
+        initRefresh()
     }
     override func viewDidAppear(_ animated: Bool) {
         // DATA
@@ -33,5 +38,23 @@ class NotificationViewController: TitleLeftViewController {
         self.navigationController?.isNavigationBarHidden = true
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
         self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
+    }
+}
+extension NotificationViewController {
+    func initRefresh() {
+        refreshControl.addTarget(self, action: #selector(refreshTable(refresh:)), for: .valueChanged)
+        
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .black
+        
+        self.notiView.notificationTableView.refreshControl = refreshControl
+    }
+    
+    @objc func refreshTable(refresh: UIRefreshControl) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // DATA reload
+            NotificationDataManager().getNotificationListDataManager(self.notiView)
+            refresh.endRefreshing()
+        }
     }
 }
