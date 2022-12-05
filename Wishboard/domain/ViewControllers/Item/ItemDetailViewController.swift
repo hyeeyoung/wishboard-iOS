2a926debce4af03dedef8edd686d75ff7055a3b3
gom1n <alice436777@gmail.com>
2022-12-05 22:56:37 +0900

    [fix] 아이템 디테일 > 화면 didAppear 마다 api 호출

--- Wishboard/domain/ViewControllers/Item/ItemDetailViewController.swift
+++ Wishboard/domain/ViewControllers/Item/ItemDetailViewController.swift
@@ -33,6 +33,7 @@ class ItemDetailViewController: UIViewController {
         ItemDataManager().getItemDetailDataManager(self.itemId, self)
     }
     override func viewDidAppear(_ animated: Bool) {
+        ItemDataManager().getItemDetailDataManager(self.itemId, self)
         // Network Check
         NetworkCheck.shared.startMonitoring(vc: self)
     }
