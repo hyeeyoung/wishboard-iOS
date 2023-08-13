ac571d4ba989bd4979cf2959bc85fa44609cc750
gom1n <alice436777@gmail.com>
2023-08-13 23:09:18 +0900

    [refactor] 링크공유 > 아이템 등록 통신 로직 수정 및 상품알림 날짜선택 Picker 커스텀

--- Share Extension/Services/ShareDataManager.swift
+++ Share Extension/Services/ShareDataManager.swift
@@ -52,33 +52,46 @@ class ShareDataManager {
         }
     }
     // MARK: - 아이템 간편 등록
-    // MARK: - 모든 데이터가 존재하는 경우 (폴더O, 알림O)
-    func uploadItemDataManager(_ folderId: Int,
+    func uploadItemDataManager(_ folderId: Int?,
                                _ photo: UIImage,
                                _ itemName: String,
                                _ itemPrice: String,
                                _ itemURL: String,
                                _ itemMemo: String,
-                               _ itemNotificationType: String,
-                               _ itemNotificationDate: String,
+                               _ itemNotificationType: String?,
+                               _ itemNotificationDate: String?,
                                _ viewcontroller: ShareViewController) {
+        
         let BaseURL = defaults?.string(forKey: "url") ?? ""
         let token = defaults?.string(forKey: "accessToken") ?? ""
+        
         let multiHeader: HTTPHeaders = [
             "Content-type": "multipart/form-data",
             "Authorization": "Bearer " + token
         ]
         
         let uploadItemUrl = BaseURL + "/item"
-        let body : Parameters = [
-                        "folder_id" : folderId,
-                        "item_name" : itemName,
-                        "item_price" : itemPrice,
-                        "item_url" : itemURL,
-                        "item_memo" : itemMemo,
-                        "item_notification_type" : itemNotificationType,
-                        "item_notification_date" : itemNotificationDate,
-                    ]    //PUT 함수로 전달할 String 데이터, 이미지 데이터는 제외하고 구성
+        
+        //PUT 함수로 전달할 String 데이터, 이미지 데이터는 제외하고 구성
+        var body: Parameters = [
+            "item_name" : itemName,
+            "item_price" : itemPrice,
+            "item_url" : itemURL,
+            "item_memo" : itemMemo,
+        ]
+        if let folderId = folderId, folderId != -1 {
+            body.updateValue(folderId, forKey: "folder_id")
+        }
+        if let itemNotificationType = itemNotificationType {
+            body.updateValue(itemNotificationType, forKey: "item_notification_type")
+        }
+        if let itemNotificationDate = itemNotificationDate {
+            /// 시간을 알맞은 형식으로 전환
+            /// 23년 09월 16일 13:30 -> 2023-09-16 13:30:00
+            let notificationDate = FormatManager().koreanStrToDate(itemNotificationDate)! + ":00"
+            body.updateValue(notificationDate, forKey: "item_notification_date")
+        }
+        
         AF.upload(multipartFormData: { (multipart) in
             if let imageData = photo.resizeImageIfNeeded().jpegData(compressionQuality: 1.0) {
                 multipart.append(imageData, withName: "item_img", fileName: "photo.jpg", mimeType: "image/jpeg")
@@ -102,164 +115,6 @@ class ShareDataManager {
                         print(result)
                     } catch {
                         print("error", data)
-                    }
-                case let .failure(error): // 요청 x
-                print(error.responseCode)
-            }
-        }
-    }
-    // MARK: - (폴더X, 알림O)
-    func uploadItemDataManager(_ photo: UIImage,
-                               _ itemName: String,
-                               _ itemPrice: String,
-                               _ itemURL: String,
-                               _ itemMemo: String,
-                               _ itemNotificationType: String,
-                               _ itemNotificationDate: String,
-                               _ viewcontroller: ShareViewController) {
-        let BaseURL = defaults?.string(forKey: "url") ?? ""
-        let token = defaults?.string(forKey: "accessToken") ?? ""
-        let multiHeader: HTTPHeaders = [
-            "Content-type": "multipart/form-data",
-            "Authorization": "Bearer " + token
-        ]
-        
-        let uploadItemUrl = BaseURL + "/item"
-        let body : Parameters = [
-                        "item_name" : itemName,
-                        "item_price" : itemPrice,
-                        "item_url" : itemURL,
-                        "item_memo" : itemMemo,
-                        "item_notification_type" : itemNotificationType,
-                        "item_notification_date" : itemNotificationDate,
-                    ]    //PUT 함수로 전달할 String 데이터, 이미지 데이터는 제외하고 구성
-        AF.upload(multipartFormData: { (multipart) in
-            if let imageData = photo.resizeImageIfNeeded().jpegData(compressionQuality: 1.0) {
-                multipart.append(imageData, withName: "item_img", fileName: "photo.jpg", mimeType: "image/jpeg")
-                //이미지 데이터를 POST할 데이터에 덧붙임
-            }
-            for (key, value) in body {
-                multipart.append("\(value)".data(using: .utf8, allowLossyConversion: false)!, withName: key)
-                //이미지 데이터 외에 같이 전달할 데이터 (여기서는 idx, name, introduction 등)
-            }
-        }
-        ,to: URL(string: uploadItemUrl)!    //전달할 url
-        ,method: .post        //전달 방식
-        ,headers: multiHeader) //헤더
-        .responseData { response in
-            switch response.result {
-                case let .success(data):
-                    do {
-                        let decoder = JSONDecoder()
-                        let result = try decoder.decode(APIModel<ResultModel>.self, from: data)
-                        viewcontroller.uploadItemAPISuccess(result)
-                        print(result)
-                    } catch {
-                        print("error", data)
-                    }
-                case let .failure(error): // 요청 x
-                print(error.responseCode)
-            }
-        }
-    }
-    // MARK: - 날짜 설정 하지 않은 경우 (폴더O, 알림X)
-    func uploadItemDataManager(_ folderId: Int,
-                               _ photo: UIImage,
-                               _ itemName: String,
-                               _ itemPrice: String,
-                               _ itemURL: String,
-                               _ itemMemo: String,
-                               _ viewcontroller: ShareViewController) {
-        let BaseURL = defaults?.string(forKey: "url") ?? ""
-        let token = defaults?.string(forKey: "accessToken") ?? ""
-        let multiHeader: HTTPHeaders = [
-            "Content-type": "multipart/form-data",
-            "Authorization": "Bearer " + token
-        ]
-        
-        let uploadItemUrl = BaseURL + "/item"
-        let body : Parameters = [
-                        "folder_id" : folderId,
-                        "item_name" : itemName,
-                        "item_price" : itemPrice,
-                        "item_url" : itemURL,
-                        "item_memo" : itemMemo,
-                    ]    //PUT 함수로 전달할 String 데이터, 이미지 데이터는 제외하고 구성
-        AF.upload(multipartFormData: { (multipart) in
-            if let imageData = photo.resizeImageIfNeeded().jpegData(compressionQuality: 1.0) {
-                multipart.append(imageData, withName: "item_img", fileName: "photo.jpg", mimeType: "image/jpeg")
-                //이미지 데이터를 POST할 데이터에 덧붙임
-            }
-            for (key, value) in body {
-                multipart.append("\(value)".data(using: .utf8, allowLossyConversion: false)!, withName: key)
-                //이미지 데이터 외에 같이 전달할 데이터 (여기서는 idx, name, introduction 등)
-            }
-        }
-        ,to: URL(string: uploadItemUrl)!    //전달할 url
-        ,method: .post        //전달 방식
-        ,headers: multiHeader) //헤더
-        .responseData { response in
-            switch response.result {
-                case let .success(data):
-                    do {
-                        let decoder = JSONDecoder()
-                        let result = try decoder.decode(APIModel<ResultModel>.self, from: data)
-                        viewcontroller.uploadItemAPISuccess(result)
-                        print(result)
-                    } catch {
-                        let str = String(decoding: data, as: UTF8.self)
-                        print("error", str)
-                    }
-                case let .failure(error): // 요청 x
-                print(error.responseCode)
-            }
-        }
-    }
-    // MARK: - 날짜 설정 하지 않은 경우 (폴더X, 알림X)
-    func uploadItemDataManager(_ photo: UIImage,
-                               _ itemName: String,
-                               _ itemPrice: String,
-                               _ itemURL: String,
-                               _ itemMemo: String,
-                               _ viewcontroller: ShareViewController) {
-        let BaseURL = defaults?.string(forKey: "url") ?? ""
-        let token = defaults?.string(forKey: "accessToken") ?? ""
-        let multiHeader: HTTPHeaders = [
-            "Content-type": "multipart/form-data",
-            "Authorization": "Bearer " + token
-        ]
-        
-        let uploadItemUrl = BaseURL + "/item"
-        let body : Parameters = [
-                        "item_name" : itemName,
-                        "item_price" : itemPrice,
-                        "item_url" : itemURL,
-                        "item_memo" : itemMemo,
-                    ]    //PUT 함수로 전달할 String 데이터, 이미지 데이터는 제외하고 구성
-        AF.upload(multipartFormData: { (multipart) in
-            if let imageData = photo.resizeImageIfNeeded().jpegData(compressionQuality: 1.0) {
-                multipart.append(imageData, withName: "item_img", fileName: "photo.jpg", mimeType: "image/jpeg")
-                //이미지 데이터를 POST할 데이터에 덧붙임
-            }
-            for (key, value) in body {
-                multipart.append("\(value)".data(using: .utf8, allowLossyConversion: false)!, withName: key)
-                //이미지 데이터 외에 같이 전달할 데이터 (여기서는 idx, name, introduction 등)
-            }
-        }
-        ,to: URL(string: uploadItemUrl)!    //전달할 url
-        ,method: .post        //전달 방식
-        ,headers: multiHeader) //헤더
-        .responseData { response in
-            switch response.result {
-                case let .success(data):
-                    do {
-                        let decoder = JSONDecoder()
-                        let result = try decoder.decode(APIModel<ResultModel>.self, from: data)
-                        viewcontroller.uploadItemAPISuccess(result)
-                        print(result)
-                    } catch {
-                        let str = String(decoding: data, as: UTF8.self)
-                        print("error", str)
                     }
                 case let .failure(error): // 요청 x
                 print(error.responseCode)
