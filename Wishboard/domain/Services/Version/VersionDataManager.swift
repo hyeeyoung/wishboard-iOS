3aa41dcbece6a3b72e0df348605f9192f57331d2
gom1n <alice436777@gmail.com>
2024-09-25 21:12:39 +0900

    [fix] version check url 변경

--- Wishboard/domain/Services/Version/VersionDataManager.swift
+++ Wishboard/domain/Services/Version/VersionDataManager.swift
@@ -16,15 +16,15 @@ class VersionDataManager {
             "Content-type": "application/json",
             "User-Agent": Storage().AgentHeader
         ]
-        AF.request(Storage().BaseURL + "/version?osType=iOS",
+        AF.request(Storage().BaseURL + "/version/check?osType=iOS",
                            method: .get,
                            parameters: nil,
                            headers: header)
             .validate()
-            .responseDecodable(of: [VersionModel].self) { response in
+            .responseDecodable(of: VersionModel.self) { response in
             switch response.result {
             case .success(let result):
-                viewcontroller.checkAppVersion(result[0])
+                viewcontroller.checkAppVersion(result)
             case .failure(let error):
                 let statusCode = error.responseCode
                 print("VERSION 가져오기 error", error.localizedDescription)
